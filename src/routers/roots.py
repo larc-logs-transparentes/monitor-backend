386ad6bab259f99e861d6fcba33e406dc53083a4
kha_khalil <hellkha@gmail.com>
2024-04-07 16:50:02 -0300

    working connection to db and fastapi routes organized

--- src/routers/roots.py
+++ src/routers/roots.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+from ..db_manager.db_collection_getter import get_db_collection
 
 router = APIRouter()
 
 
-@router.get("/roots/", tags=["roots"])
-async def read_users():
-    return [{"root": "my_root_1"}, {"root": "my_root_2"}]
+# Hard coded endpoint for tests
+@router.get("/all_roots/")
+async def no_year():
+    return {"message": "No year selected"}
+
+
+# Get all roots from election in year
+@router.get("/all_roots/{year}")
+async def get_documents(year):
+    collection = get_db_collection(year)
+    documents = collection.find({})
+    results = []
+    for doc in documents:
+        results.append(doc.get('root'))
+    return results
