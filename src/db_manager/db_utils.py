b7ecc7f2d8afa80b05ed7cd87f97e1f5cd49de6b
kha_khalil <hellkha@gmail.com>
2024-05-25 19:16:25 -0300

    db_utils: added function to access db roots: added routes to check one root, post for receiving whole object and get for receiving only year and value routers/helper: function to compare roots

--- src/db_manager/db_utils.py
+++ src/db_manager/db_utils.py
@@ -9,18 +9,25 @@ def get_db_collection(db_name, collection_name):
     return collection
 
 
+def select_all_docs_in_year(collection_name, db_name):
+    collection = get_db_collection(db_name, collection_name)
+    documents = collection.find({})
+    results = []
+    for doc in documents:
+        doc['_id'] = str(doc['_id'])
+        results.append(doc)
+    return results
+
+
 def insert_roots_to_collection(collection_name, db_name, roots):
-    client = get_db_client()
-    db = client[db_name]
-    collection = db[collection_name]
+    collection = get_db_collection(db_name, collection_name)
     result = collection.insert_many(roots)
     return result
 
 
+# Select from given db collection the document in which property "tree_size" is bigger
 def select_document_larger_tree_size(collection_name, db_name):
-    client = get_db_client()
-    db = client[db_name]
-    collection = db[collection_name]
+    collection = get_db_collection(db_name, collection_name)
     pipeline = [
         {"$sort": {"tree_size": -1}},
         {"$limit": 1}
@@ -30,3 +37,33 @@ def select_document_larger_tree_size(collection_name, db_name):
         return result[0]
     else:
         return {}
+
+
+def select_document_with_tree_size(collection_name, db_name, tree_size):
+    collection = get_db_collection(db_name, collection_name)
+    documents = collection.find({"tree_size": tree_size})
+    result = stringify_docs_ids(documents)
+    if len(result) > 0:
+        return result[0]
+    else:
+        return {}
+
+
+# Select document from db with given root value
+def select_document_with_value(collection_name, db_name, value):
+    collection = get_db_collection(db_name, collection_name)
+    documents = collection.find({"value": value})
+    result = stringify_docs_ids(documents)
+    if len(result) > 0:
+        return result[0]
+    else:
+        return {}
+
+
+# Transform Mongo ID object to string
+def stringify_docs_ids(documents):
+    results = []
+    for doc in documents:
+        doc['_id'] = str(doc['_id'])
+        results.append(doc)
+    return results
